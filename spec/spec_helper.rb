6ee4496dd1b8c32ddfbb20582e67ae92d1785d62
Josh Mills <josh@trueheart78.com>
2017-10-03 14:17:02 -0400

    Simplecov setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require "bundler/setup"
 require "gifs"
+require "support/simplecov"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
---
+++ spec/support/simplecov.rb
@@ -0,0 +1,6 @@
+require 'simplecov'
+require 'simplecov-rcov'
+
+SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+SimpleCov.minimum_coverage 93
+SimpleCov.start
