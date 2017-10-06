bf4efd1fb58a5eb6d32f974ebf8d8e6812a6e170
Josh Mills <josh@trueheart78.com>
2017-10-06 11:52:17 -0400

    Directory now supports criteria - needs specs

--- lib/gifs/directory.rb
+++ lib/gifs/directory.rb
@@ -1,5 +1,9 @@
 module Gifs
   class Directory
+    def initialize(criteria: [])
+      @criteria = criteria
+    end
+
     def to_s
       "#{humanize(gif_count)} gifs in #{humanize(dir_count)} directories"
     end
@@ -13,14 +17,54 @@ module Gifs
     end
 
     def gifs
-      @gifs ||= Dir[File.join(dir, '**', '*.gif')].reject { |p| File.directory? p }
+      return [] if gif_paths.empty?
+      @gifs ||= Dir[*gif_paths].reject { |p| File.directory? p }
     end
 
     def dirs
-      @dirs ||= Dir[File.join(dir, '**', '*')].select { |p| File.directory? p }
+      return [] if dir_paths.empty?
+      return criteria_dirs if criteria.any?
+      @dirs ||= Dir[*dir_paths].select { |p| File.directory? p }
     end
 
     private
+
+    attr_reader :criteria
+
+    def gif_paths
+      @gif_paths ||= search_paths('*.gif')
+    end
+
+    def dir_paths
+      @dir_paths ||= search_paths('*')
+    end
+
+    def search_paths(file_match)
+      glob_patterns.map { |p| File.join(dir, p, file_match) }
+    end
+
+    def glob_patterns
+      return ['**'] if criteria.empty?
+      @glob_patterns ||= valid_criteria.map { |c| "**/*#{c}*/**" }
+    end
+
+    def criteria_dirs
+      return [] if valid_criteria.empty?
+      @criteria_dirs ||= unique_criteria_dirs
+    end
+
+    def valid_criteria
+      criteria.select { |c| File.directory?(File.join(dir, c)) }
+    end
+
+    def unique_criteria_dirs
+      gifs.map { |v| v.gsub(dir + '/', '')
+        .split('/')
+        .reject { |v| v.end_with? '.gif' } }
+        .flatten
+        .uniq
+        .map { |v| File.join(dir, v) }
+    end
 
     def dir
       raise "No environment var provided for #{Gifs::ENV_DIR_KEY}" unless ENV[Gifs::ENV_DIR_KEY]
